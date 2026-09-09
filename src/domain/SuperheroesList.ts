048f1c2ef1b64a4e4548cdb96c9c102c3eccb1bc
snowydevd <lautarochini4@gmail.com>
2026-09-09 18:39:28 -0300

    fix heroes

--- src/domain/SuperheroesList.ts
+++ src/domain/SuperheroesList.ts
@@ -8,8 +8,8 @@ export class SuperheroesList {
     { name: "Thanos", side: "villain" },
     { name: "Loki", side: "villain" },
     { name: "Wanda Maximoff", side: "villain" },
-    { name: "Ultron", side: "villain" },
-    { name: "Red Skull", side: "villain" },
+    { name: "Black Widow", side: "hero" },
+    { name: "Hawkeye", side: "hero" },
   ];
 
   getHeroes(): Superhero[] {
